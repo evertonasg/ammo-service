da170e36647d884327db25c89ec2ff61fa1ee2e5
Everton Guimarães <everton.asg@gmail.com>
2018-09-23 20:18:24 -0300

    all commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var app = require('./config/custom-express')();
 var server = require('http').createServer(app);
-var porta = 3002;
+var porta = process.env.PORT || 3002;
 
 server.listen(porta, function(){
     console.log("rodando na porta " + porta + "...");
